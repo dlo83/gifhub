398cb9ace64956203fa3550565610877b88e5388
Dan Lourenco <dan.lourenco@nike.com>
2018-11-02 13:39:40 -0700

    autofocus

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.inputRef = React.createRef();
+  }
+  componentDidUpdate() {
+    this.inputRef.focus();
+  }
+
   render() {
     const { onChange, onSearch, value } = this.props;
     return (
       <div className="searchbar">
         <h2>GifHub</h2>
         <input
+          ref={ this.inputRef }
           autoFocus={ true }
           className="searchbox"
           type="text"
