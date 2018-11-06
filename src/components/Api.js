334290b1cb4f7431e3662c95a589cd92df08c38e
Dan Lourenco <danlourenco@gmail.com>
2018-11-05 22:23:41 -0800

    Change defaults (#8)
    * update to babel 7, move shit around
    
    * update babel
    
    * update defaults

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,7 @@
 const API_KEY = '6pru14UvLOEhi06VmrSebbDCbTnyS6Ry';
 const baseUrl = `https://api.giphy.com/v1/gifs/search?api_key=${ API_KEY }`;
 
-export const search = ({query, limit, rating}) => {
+export const search = ({query, limit = '25', rating = 'G'}) => {
   return fetch(`${ baseUrl }&q=${ query }&limit=${ limit }&offset=0&rating=${ rating }&lang=en`)
   .then(res => res.json());
 }
